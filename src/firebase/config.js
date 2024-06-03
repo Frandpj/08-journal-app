b7212fad3bb4c886861b7c3c0c1802393dfec150
Fran_dpj <fdelpinojimenez@gmail.com>
2024-06-03 09:19:16 +0200

    Fin de test

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,9 +2,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
+import { getEnvironments } from '../helpers/getEnvironments';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+const {
+    VITE_APIKEY,
+    VITE_AUTHDOMAIN,
+    VITE_DATABASEURL,
+    VITE_PROJECTID,
+    VITE_STORAGEBUCKET,
+    VITE_MESSAGINGSENDERID,
+    VITE_APPID,
+  } = getEnvironments();
 
 // dev/pro
 // const firebaseConfig = {
@@ -17,14 +28,14 @@ import { getFirestore } from "firebase/firestore/lite";
 // };
 
 // Testing
-const firebaseConfig = {
-    apiKey: "AIzaSyCv-xzcF2Ik9KBBVECTJczLplV3QZuHKdA",
-    authDomain: "react-firebase-testing-390b3.firebaseapp.com",
-    projectId: "react-firebase-testing-390b3",
-    storageBucket: "react-firebase-testing-390b3.appspot.com",
-    messagingSenderId: "635825328538",
-    appId: "1:635825328538:web:1063519778527a22d22a23"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCv-xzcF2Ik9KBBVECTJczLplV3QZuHKdA",
+//     authDomain: "react-firebase-testing-390b3.firebaseapp.com",
+//     projectId: "react-firebase-testing-390b3",
+//     storageBucket: "react-firebase-testing-390b3.appspot.com",
+//     messagingSenderId: "635825328538",
+//     appId: "1:635825328538:web:1063519778527a22d22a23"
+// };
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
